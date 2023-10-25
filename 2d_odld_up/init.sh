a466651e7d0719d94055e20612482c56fc21ccd5
Darian T. Yang <dty7@pitt.edu>
2023-10-25 13:19:07 -0400

    added 2d odld test systems

---
+++ 2d_odld_up/init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rm -f west.h5 binbounds.txt
+#BSTATES="--bstate initial,1,9.5"
+#TSTATES="--tstate final,1.9,1.9"
+
+#w_init $BSTATES $TSTATES "$@"
+#w_init $BSTATES "$@"
+
+#w_init $BSTATES $TSTATES --segs-per-state 50 "$@"
+#w_init $BSTATES --segs-per-state 50 "$@"
+
+#BSTATES="--bstate-file bstates.txt"
+#TSTATES="--tstate-file tstates.txt"
+#w_init $BSTATES $TSTATES "$@"
+
+rm -f west.h5 binbounds.txt
+#BSTATES="--bstate-file bstates.txt"
+BSTATES="--bstate start,1,0.1"
+TSTATES="--tstate final,2.51,2.51"
+#w_init --segs-per-state 40 $BSTATES $TSTATES "$@"
+w_init $BSTATES $TSTATES "$@"
--- odld/init.sh
+++ odld/init.sh
@@ -7,5 +7,8 @@ TSTATES="--tstate final,1.9"
 #w_init $BSTATES $TSTATES "$@"
 #w_init $BSTATES "$@"
 
+#BSTATES="--bstate-file bstates.txt"
+#TSTATES="--tstate-file tstates.txt"
+
 #w_init $BSTATES $TSTATES --segs-per-state 50 "$@"
 w_init $BSTATES --segs-per-state 50 "$@"
