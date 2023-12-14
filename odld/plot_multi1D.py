5f75d467c2d1cdd95dc91bb31f2a8f28866c368b
Darian T. Yang <dty7@pitt.edu>
2023-12-14 12:30:01 -0500

    got the energy fixed since I had wrong constant, after 3000i converged at low S

--- odld/plot_multi1D.py
+++ odld/plot_multi1D.py
@@ -2,7 +2,7 @@ import wedap
 import matplotlib.pyplot as plt
 import numpy as np
 
-h5_list = ["hk1b.h5", "hk10b.h5", "mab10b.h5", "wevo.h5", "lcas.h5", "west_lowS.h5"]
+h5_list = ["hk1b.h5", "hk10b.h5", "mab10b.h5", "wevo.h5", "lcas.h5"]
 #h5_list = ["west_lowS.h5"]
 
 fig, ax = plt.subplots()
@@ -10,6 +10,13 @@ fig, ax = plt.subplots()
 for h5 in h5_list:
     wedap.H5_Plot(h5=h5, data_type="average", first_iter=250, plot_mode="line", 
                   data_label=h5, ax=ax).plot()
+
+    # wedap.H5_Plot(h5=h5, data_type="average", last_iter=1000, plot_mode="line", 
+    #               data_label="1000i", ax=ax).plot()
+    # wedap.H5_Plot(h5=h5, data_type="average", first_iter=1000, last_iter=2000, plot_mode="line", 
+    #               data_label="1-2000i", ax=ax).plot()
+    # wedap.H5_Plot(h5=h5, data_type="average", first_iter=2000, plot_mode="line", 
+    #               data_label="2-3000i", ax=ax).plot()
 
 def plot_odld(A=2, B=5, C=0.5, x0=1):
     twopi_by_A = 2 * np.pi / A
@@ -26,10 +33,10 @@ def plot_odld(A=2, B=5, C=0.5, x0=1):
     y = half_B / (eCx_less_one * eCx_less_one) * (twopi_by_A * eCx_less_one * np.sin(xarg) + C * eCx * np.cos(xarg))
 
     #plt.plot(x, np.flip(-np.log(y)), color='k', alpha=0.5, label='ODLD potential', linestyle="--")
-    plt.plot(x, y, color='k', alpha=0.5, label='ODLD potential', linestyle="--")
+    plt.plot(x, y, color='k', alpha=0.5, label='ODLD grad', linestyle="--")
 #plot_odld()
 
-def odld_1d_potential(A=2, B=10, C=0.5, x0=0):
+def odld_1d_potential(A=2, B=5, C=0.5, x0=1):
     x = np.arange(0.5,10.1,0.05) 
     twopi_by_A = 2 * np.pi / A
     half_B = B / 2
@@ -42,11 +49,9 @@ def odld_1d_potential(A=2, B=10, C=0.5, x0=0):
     potential = -half_B / eCx_less_one * np.cos(xarg)
 
     # normalize the plot to have lowest value as baseline
-    potential = potential + np.min(potential)
-    #print(np.min(potential))
+    potential = potential - np.min(potential)
 
-    # negative plot is accurate to the simulation results
-    plt.plot(x, -potential, color='k', alpha=0.5, label='ODLD potential', linestyle="--")
+    plt.plot(x, potential, color='k', alpha=0.5, label='ODLD potential', linestyle="--")
     return potential
 odld_1d_potential()
 
